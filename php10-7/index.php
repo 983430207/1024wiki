d43747f20983c178765c594eb8e2ac6bf95d122c
bobi <983430207@qq.com>
2019-11-07 14:00:22 +0800

    第二天，编写留言板网页

--- php10-7/index.php
+++ php10-7/index.php
@@ -1,1 +1,37 @@
-12345
+<html>
+    <head>
+        <title>留言板-1024编程实验室</title>
+        <meta charset='utf-8' />
+        <style>
+            .add, .list{width:800px; margin:0 auto;}
+            textarea{width:100%; height:100px; margin-bottom:10px;}
+            .username{float:left;}
+            .submit{float:right;}
+            .msg{border:solid 1px #cccccc; margin-top:10px;padding:5px;}
+        </style>
+    </head>
+    <body>
+        <div class='add'>
+            <textarea></textarea>
+            <input class='username' type='text' />
+            <input class='submit' type='submit' value='提交'/>
+            <div style='clear:both;'></div>
+        </div>
+
+        <div class='list'>
+            <div class='msg'>
+                <p>用户名</p>
+                <p>留言内容</p>
+            </div>
+            <div class='msg'>
+                <p>用户名</p>
+                <p>留言内容</p>
+            </div>
+            <div class='msg'>
+                <p>用户名</p>
+                <p>留言内容</p>
+            </div>
+        </div>
+
+    </body>
+</html>
