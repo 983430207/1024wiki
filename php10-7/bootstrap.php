e00c07c2fe625dbd740dcb12729033c78f80efb8
bobi <983430207@qq.com>
2019-11-19 10:54:46 +0800

    第12天 实现数据分页

--- php10-7/bootstrap.php
+++ php10-7/bootstrap.php
@@ -1,6 +1,20 @@
 <?php
 include('db.php');
-$sql = "SELECT * FROM `msg` ORDER BY id DESC";
+
+//获取数据总数
+$sql = "select count(*) as t from msg";
+$total = $db->read($sql)[0]['t'];
+
+//每页显示
+$pageNum = 5;
+$pageMax = ceil($total / $pageNum);
+
+//计算当前应该显示哪些数据
+$page = $_GET['page'] ?? 1;
+
+$offset = ($page-1)*$pageNum; // 1*5;
+
+$sql = "SELECT * FROM `msg` ORDER BY id DESC LIMIT $offset,5";
 
 $rows = $db->read($sql);
 ?>
@@ -25,39 +39,54 @@ $rows = $db->read($sql);
             <p class="lead">根据1024编程实验室做的入门程序，请大家鉴赏。</p>
         </div>
         <form action="save.php" method='POST'>
-        <div class='row'>
-        
-            <div class='col-12'>
-                <div class="form-group">
-                    <textarea name='content' class="form-control" rows='4'></textarea>
+            <div class='row'>
+
+                <div class='col-12'>
+                    <div class="form-group">
+                        <textarea name='content' class="form-control" rows='4'></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class='col-3'>
-                <div class="form-group">
-                    <input name='username' class='form-control' type='text' />
+                <div class='col-3'>
+                    <div class="form-group">
+                        <input name='username' class='form-control' type='text' />
+                    </div>
                 </div>
-            </div>
-            <div class='col-9 d-flex'>
-                <div class="form-group ml-auto">
-                    <input class='btn btn-primary' type='submit' value='提交' />
+                <div class='col-9 d-flex'>
+                    <div class="form-group ml-auto">
+                        <input class='btn btn-primary' type='submit' value='提交' />
+                    </div>
                 </div>
+
             </div>
-        
-        </div>
         </form>
 
         <div class='row'>
             <div class='col-12'>
-            <?php
-            foreach($rows as $key=>$msg){ 
-            ?>
-                <div class='border rounded p-2 mb-2'>
-                    <div class='text-primary'><?php echo $msg['username'];?></div>
-                    <div><?php echo $msg['content'];?></div>
-                </div>
-            <?php
-            }
-            ?>
+                <?php
+                foreach ($rows as $key => $msg) {
+                    ?>
+                    <div class='border rounded p-2 mb-2'>
+                        <div class='text-primary'><?php echo $msg['username']; ?></div>
+                        <div><?php echo $msg['content']; ?></div>
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-12'>
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <?php for ($i = 1; $i <= $pageMax; $i++) : ?>
+                            <li class="page-item">
+                                <a class="page-link" href="bootstrap.php?page=<?php echo $i; ?>">
+                                    <?php echo $i; ?>
+                                </a>
+                            </li>
+                        <?php endfor; ?>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
